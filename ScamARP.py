157f906cbb69fd103799dccba78ff387477e0867
YHerm <0549405422.yh@gmail.com>
2025-04-08 15:59:04 +0300

    rm comment, add return type

--- ScamARP.py
+++ ScamARP.py
@@ -19,14 +19,14 @@ def is_arp_reply(packet: Packet) -> bool:
     return packet.haslayer(ARP) and packet[ARP].op == ArpType.REPLY.value
 
 
-def build_arp_msg(arp_type: ArpType, src_mac: str, src_ip: str, dst_mac: str, dst_ip: str):
+def build_arp_msg(arp_type: ArpType, src_mac: str, src_ip: str, dst_mac: str, dst_ip: str) -> ARP:
     return ARP(op=arp_type.value, hwsrc=src_mac, psrc=src_ip, hwdst=dst_mac, pdst=dst_ip, )
 
 
 def handle_arp_packet(packet: Packet) -> None:
-    # if is_arp_reply(packet):
-    #     print(f"ARP Reply from {packet[ARP].psrc} ({packet[ARP].hwsrc}) asking for {packet[ARP].pdst}")
-    #     return
+    if is_arp_reply(packet):
+        print(f"ARP Reply from {packet[ARP].psrc} ({packet[ARP].hwsrc}) asking for {packet[ARP].pdst}")
+        return
 
     if is_arp_request(packet):
         print(f"ARP Request from {packet[ARP].psrc} ({packet[ARP].hwsrc}) asking for {packet[ARP].pdst}")
